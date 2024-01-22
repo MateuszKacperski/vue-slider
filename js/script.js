c0aef7bc39019dd77b3da0704365cd159d52eb70
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-22 16:56:41 +0100

    added bonus

--- js/script.js
+++ js/script.js
@@ -3,13 +3,36 @@ console.log('Vue ok', Vue);
 const {createApp} = Vue;
 const app = createApp({
     name: 'Vue Slider',
-    data(){
-        return {
+    data: () => ({
+       
             pictures,
             currentIndex: 0,
+            interval:''
+    }),
+    methods: {
+        goNext(){
+            if(this.currentIndex === pictures.length-1){
+                this.currentIndex = 0;
+            }else this.currentIndex++;
 
+            
+        },
+        goPrev(){
+            if(this.currentIndex === 0){
+                this.currentIndex = pictures.length-1;
+            }else this.currentIndex--;
+        },
+        stopInterval(){
+            clearInterval(this.interval)
+        },
+        startInterval(){
+            setInterval(this.goNext, 3000);
         }
+    },
+    mounted(){
+        this.interval = setInterval(this.goNext, 3000);
     }
-
 })
+
+
 app.mount('#root')
