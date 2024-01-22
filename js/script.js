a49c7aa81f1a08df7ee0331d7dd8e0fde9738aab
MateuszKacperski <kacperskimateusz95@gmail.com>
2024-01-22 14:51:26 +0100

    added thumbnails

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 console.log('Vue ok', Vue);
+
+const {createApp} = Vue;
+const app = createApp({
+    name: 'Vue Slider',
+    data(){
+        return {
+            pictures,
+            currentIndex: 0,
+
+        }
+    }
+
+})
+app.mount('#root')
